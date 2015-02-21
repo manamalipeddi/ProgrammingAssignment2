89b26f3b16f29f0a47e3512a9dc6fc32cfbd183a
Manasa Malipeddi <mana.malipeddi@gmail.com>
2015-02-21 18:18:10 +0530

    Added R Code for Peer Review
    Coursera's R Programming Assignment for Peer Review -- Inverse of a
    Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Final R Code for Computing Matrix Inverse using Lexical Scoping
+#Code written by Manasa Malipeddi for the Coursera R Programming Course
+#Programming Assignment 2 for peer review
 
-## Write a short comment describing this function
+#The following functions compute the inverse of a matrix using lexical scoping
+#makeCacheMatrix defines functions that access and modify cached variables returning a list of these functions
+#cacheSolve uses the functions defined in makeCacheMatrix and computes to inverse of any nxn matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#this function computes the inverse of a square matrix
+cacheSolve <- function(my_matrix, ...) {
+        
+        #if same matrix as last time is passed, inverse already exists, print that
+        cached_matrix <- my_matrix$getmatrixincache()
+        new_matrix <- my_matrix$getmatrix()
+        if(identical(new_matrix, cached_matrix)) {
+                message("Same as previous matrix. Retrieving inverse from cache...")
+                matrix_inverse <- my_matrix$getinverse()
+        }
+        else {
+                #compute new inverse for the new matrix
+                matrix_inverse <- solve(new_matrix, ...)
+                #save new matrix and new inverse computed into cache for future
+                my_matrix$setmatrixincache(new_matrix)
+                my_matrix$setinverse(matrix_inverse)         
+        }
+        
+        #finally return inverse matrix to the console
+        matrix_inverse
 }
 
+#this function creates functions to manipulate cache variables
+makeCacheMatrix <- function(my_matrix=matrix()) {
+        #initialize variables
+        my_matrix_cache <- NULL
+        matrix_inverse_cache <- NULL
+        
+        #the set function stores new matrix 
+        setmatrix <- function(new_matrix) my_matrix <<- new_matrix        
+        # get new matrix 
+        setmatrixincache <-  function(new_matrix) my_matrix_cache <<- new_matrix
+        #get new matrix
+        getmatrix <- function() my_matrix
+        # get cached old matrix
+        getmatrixincache <- function() my_matrix_cache
+        #this function sets the inverse computed into cache for future use
+        setinverse <- function(matrix_inverse) matrix_inverse_cache <<- matrix_inverse
+        #this function returns the inverse already computed
+        getinverse <- function() matrix_inverse_cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #return the list of functions we just defined to the calling function
+        list(setmatrix = setmatrix, getmatrix = getmatrix, setinverse = setinverse, 
+             getinverse = getinverse, getmatrixincache = getmatrixincache, setmatrixincache = setmatrixincache)
 }
